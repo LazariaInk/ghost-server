fc53947b537756f6e8dd36ba166a0f0e34535777
LazariaInk <lorienlored@gmail.com>
2025-08-06 23:30:11 +0300

    init empty project with websocket, webflux and security

---
+++ src/main/java/com/lazari/ghost_server/GhostServerApplication.java
@@ -0,0 +1,13 @@
+package com.lazari.ghost_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhostServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhostServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lazari/ghost_server/GhostServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazari.ghost_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhostServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
